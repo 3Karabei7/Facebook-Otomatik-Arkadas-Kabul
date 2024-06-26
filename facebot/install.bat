4a741783968c05a81733147ea960c4e8cd963e0c
mucahit0406 <abdullahmucahit0640@gmail.com>
2024-06-26 20:07:07 +0300

    Update install.bat

--- facebot/install.bat
+++ facebot/install.bat
@@ -5,4 +5,10 @@ pip install pyautogui selenium
 
 echo PyAutoGUI ve Selenium yüklendi.
 
+echo PyInstaller ile facebots.py dosyasını derliyor...
+
+pyinstaller --onefile facebots.py
+
+echo facebots.py dosyası başarıyla derlendi.
+
 pause
