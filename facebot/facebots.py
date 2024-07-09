b07dc4d053291f8868fdcc52cf0301871ebf860d
3Karabei7 <155747071+3Karabei7@users.noreply.github.com>
2024-07-09 03:29:32 +0300

    Some change
    i changed some code

--- facebot/facebots.py
+++ facebot/facebots.py
@@ -1,112 +1,143 @@
 import sys
 import time
-import pyautogui
+import locale
+import warnings
+import colorama
+from colorama import Fore
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+# Dillerin seçeneklerini güncelleyelim
+menu_options = {
+    'de': f"""
+[{Fore.GREEN}Willkommen {Fore.YELLOW}bitte {Fore.RESET}wählen Sie die gewünschte Prozedur aus.]
+    1 - Automatische Freundschaftsanfragen annehmen.
 
-print("""HOŞGELDİNİZ LÜTFEN YAPTIRMAK İSTEDİĞİNİZ İŞLEMİ SEÇİNİZ
+    2 - {Fore.RED}Automatische Freundschaftsanfragen senden -> (In Wartung){Fore.RESET}
+""",
+    'tr': f"""
+[{Fore.GREEN}Hoş geldiniz {Fore.YELLOW}lütfen {Fore.RESET}yapmak istediğiniz işlemi seçiniz.]
+    1 - Otomatik arkadaş isteğini kabul et.
 
-1) Otomatik Arkadaş Kabul(Arkaplanda)
+    2 - {Fore.RED}Otomatik arkadaş isteği gönder --> (Bakımda){Fore.RESET}
+""",
+    'en': f"""
+[{Fore.GREEN}Welcome {Fore.YELLOW}please {Fore.RESET}select the procedure you want to perform.]
+    1 - Accept automatic friend requests.
 
-2) Otomatik Arkadaş İsteği Atma(Bakımda)
+    2 - {Fore.RED}Send automatic friend requests --> (In maintenance){Fore.RESET}
+"""
+}
 
+login_options = {
+    'de': {1: 'Facebook geöffnet!', 2: 'Bitte geben Sie die E-Mail-Adresse Ihres Kontos ein: ',
+           3: 'Bitte geben Sie Ihr Kontopasswort ein:'},
+    'tr': {1: 'Facebook açıldı!', 2: 'Lütfen hesabınızın e-posta adresini girin: ',
+           3: 'Lütfen hesabınızın şifresini girin:'},
+    'en': {1: 'Facebook is opened!', 2: 'Please enter your account\'s email address: ',
+           3: 'Please enter your account password: '}
+}
 
-""")
-secim = input("1 Yada 2: ")
-arkadas_kabul = 0
-arkadas_ekle = 0
-if secim == "1":
-    arkadas_kabul = 1
-elif secim == "2":
-    arkadas_ekle = 1
-#Geckodriver Dosya yolunu belirtiniz
-path = ''
-service = Service(path)
+exit_options = {
+    'de': "Beenden",
+    'tr': "Çıkılıyor!",
+    'en': "Exiting"
+}
 
-firefox_options = webdriver.FirefoxOptions()
-#firefox dosya yolunu belirtiniz
-firefox_options.binary_location = ""
-firefox_options.add_argument('-headless')  # Arka planda çalışır.
-browser = webdriver.Firefox(service=service, options=firefox_options)
+class Facebook:
+    ACCEPT = 1
+    SEND = 2
 
-def login_to_facebook():
-    browser.get("https://tr-tr.facebook.com/?_=_")
-    time.sleep(2)
-    print("Facebook açıldı!")
-    pyautogui.hotkey('ctrl', 'm')
-    
+class FacebookAutomation:
+    def __init__(self):
+        # Initialize colorama
+        colorama.init()
 
+        # Define the preferences
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        options.binary_location = r'127.0.2\firefox.exe'
+        self.driver = webdriver.Firefox(service=Service("geckodriver.exe"), options=options)
+        #Service(GeckoDriverManager().install())
 
-    try:
-        button = browser.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/input[1]")
-        print("Buton bulundu!")
-    except Exception as e:
-        print("Buton bulunamadı!", e)
+        # Get the language code from the locale
+        self.language_code = locale.getdefaultlocale()[0].split('_')[0]  # Use default locale
 
-    # Giriş bilgilerini bulup giriyoruz
-    kullanicigirismail = input("Lütfen Hesabınızın mail adresini giriniz: ")
-    kullanicigirissifre = input("Lütfen hesabınızın şifresini giriniz: ")
-    kullanici_mail_giris = browser.find_element(By.XPATH, "//input[@id='email']")
-    kullanici_sifre_giris = browser.find_element(By.XPATH, "//input[@id='pass']")
-    print("Giriş noktası bulundu")
-    time.sleep(1)
+    def FacebookLogin(self):
+        driver = self.driver
+        driver.get("https://www.facebook.com/")
+        driver.maximize_window()
 
-    kullanici_mail_giris.send_keys(kullanicigirismail)
-    kullanici_sifre_giris.send_keys(kullanicigirissifre)
+        print(login_options.get(self.language_code, login_options['en'])[1])
 
-    # Giriş butonuna tıklıyoruz
-    login_button = browser.find_element(By.XPATH, "//button[@name='login']")
-    login_button.click()
-    print("Giriş butonuna tıklandı!")
-    time.sleep(2)
-    current_url = browser.current_url
-    if "https://www.facebook.com/" in current_url:
-        print("Facebook'tasınız.")
-    else:
-        login_to_facebook()
+        try:
+            mail = input(login_options.get(self.language_code, login_options['en'])[2])
+            pasw = input(login_options.get(self.language_code, login_options['en'])[3])
 
-    browser.get("https://www.facebook.com/friends")
-    time.sleep(2)
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'email')))
+            email = driver.find_element(By.ID, 'email')
 
-def arkadas_kabul_etme(xpath, index):
-    try:
-        arkadas_kabul = browser.find_element(By.XPATH, xpath)
-        arkadas_kabul.click()
-        print(f"Arkadaş kabul edildi {index}")
-        time.sleep(0.5)
-    except Exception as e:
-        print(f"Arkadaş kabul edilemedi {index}:", e)
-xpath_listesi = [
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[7]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[8]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[9]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[10]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[11]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]",
-    "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[12]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/span[1]"
-]
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'pass')))
+            password = driver.find_element(By.ID, 'pass')
 
-login_to_facebook()
+            email.send_keys(mail)
+            password.send_keys(pasw)
 
-if arkadas_kabul == 1:
-    while True:
-        for i, xpath in enumerate(xpath_listesi):
-            arkadas_kabul_etme(xpath, i + 1)
-            if (i + 1) % 11 == 0:
-                print("11 arkadaş kabul edildi, sayfa yenileniyor...")
-                browser.refresh()
-                time.sleep(2)
-                break
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'login')))
+            login_button = driver.find_element(By.NAME, 'login')
+            login_button.click()
 
-# Tarayıcıyı kapat
-browser.quit()
+        except Exception as e:
+            print(f"Error during login: {e}")
 
-#bu kod Mücahit(Blass) Tarafından Yazılmıştır.
+    def FacebookAccept(self):
+        driver = self.driver
+        i = 0
+        while True:
+            try:
+                i+=1
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,
+                                                                            '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[4]/div/div/a/div[1]/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div[1]')))
+                confirm = driver.find_element(By.XPATH,
+                                           '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[4]/div/div/a/div[1]/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div[1]')
+                confirm.click()
+                driver.refresh()
+                time.sleep(0.4)
+                print(f"{i} request accepted")
+            except Exception as e:
+                print("All friends accepted or an error occurred.")
+                print("Alle Freunde akzeptiert oder ein Fehler ist aufgetreten.")
+                print("Tüm arkadaşlar kabul edildi veya bir hata oluştu.")
+
+    def FacebookSend(self):
+        print(f"{Fore.RED}{exit_options.get(self.language_code, exit_options['en'])}{Fore.RESET}")
+        self.driver.quit()
+        sys.exit()
+
+    def menu(self):
+        self.FacebookLogin()
+        self.driver.get("https://www.facebook.com/friends/requests")
+
+        print(menu_options.get(self.language_code, menu_options['en']))
+
+        try:
+            user_input = int(input(">"))
+            if user_input == Facebook.ACCEPT:
+                self.FacebookAccept()
+            elif user_input == Facebook.SEND:
+                self.FacebookSend()
+            else:
+                print(f"{Fore.RED}Invalid option selected.{Fore.RESET}")
+        except ValueError:
+            print(f"{Fore.RED}Please enter a valid number.{Fore.RESET}")
+
+FaceAuto = FacebookAutomation()
+FaceAuto.menu()
+
+# bu kod Mücahit(Blass) Tarafından Yazılmıştır.
+# Bu kod Muhammed(Karabei) tarafından düzenlenmiştir.
